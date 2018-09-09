2422f5fd79619bd32f88fae38e377c00d423918e
Bastt <ronilsondaconceicao@gmail.com>
2018-09-09 18:18:01 -0300

    request permission to use the camera

--- SJ90/Resources/AppDelegate.swift
+++ SJ90/Resources/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let viewController = TakePhotosViewController(nibName: "TakePhotosViewController", bundle: nil)
-        let navigationController = UINavigationController()
-        navigationController.viewControllers = [viewController]
-        self.window!.rootViewController = navigationController
+//        let navigationController = UINavigationController()
+//        navigationController.viewControllers = [viewController]
+        self.window!.rootViewController = viewController
         self.window?.makeKeyAndVisible()
         return true
     }
