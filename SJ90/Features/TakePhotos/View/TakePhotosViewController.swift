03993db32707b94d4bcae20a9cb948daf5c02d32
Bastt <ronilsondaconceicao@gmail.com>
2018-09-09 17:03:30 -0300

    Camera Button

---
+++ SJ90/Features/TakePhotos/View/TakePhotosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TakePhotosViewController.swift
+//  SJ90
+//
+//  Created by Ronilson Batista on 09/09/2018.
+//  Copyright © 2018 Ronilson Batista. All rights reserved.
+//
+
+import UIKit
+
+class TakePhotosViewController: UIViewController {
+    
+    @IBOutlet weak fileprivate var takePhotoButton: CameraButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("A Primeira tela")
+
+        // Do any additional setup after loading the view.
+    }
+}
