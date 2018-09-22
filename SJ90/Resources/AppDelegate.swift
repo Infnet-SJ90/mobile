0403a2008f871e7543599fb81109ee8c7030818e
ronilsonbatista <ronilsondaconceicao@gmail.com>
2018-09-22 00:54:56 -0300

    flow to report

--- SJ90/Resources/AppDelegate.swift
+++ SJ90/Resources/AppDelegate.swift
@@ -12,12 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let viewController = MainViewController(nibName: "MainViewController", bundle: nil)
-//        let navigationController = UINavigationController()
-//        navigationController.viewControllers = [viewController]
         self.window!.rootViewController = viewController
         self.window?.makeKeyAndVisible()
         return true
