39fcedc7cfec5bc91b3e516c59d12138ff50c3c1
ronilsonbatista <ronilsondaconceicao@gmail.com>
2018-09-22 01:03:08 -0300

    creating classes

---
+++ SJ90/Features/Complaint/MakeComplaints/MakeComplaintsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MakeComplaintsViewController.swift
+//  SJ90
+//
+//  Created by Ronilson Batista on 22/09/2018.
+//  Copyright © 2018 Ronilson Batista. All rights reserved.
+//
+
+import UIKit
+
+class MakeComplaintsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+}
---
+++ SJ90/Features/Complaint/MyComplaints/MyComplaintsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MyComplaintsViewController.swift
+//  SJ90
+//
+//  Created by Ronilson Batista on 22/09/2018.
+//  Copyright © 2018 Ronilson Batista. All rights reserved.
+//
+
+import UIKit
+
+class MyComplaintsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
